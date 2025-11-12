0cf8f66e879003d67a22752594684b7c06f175f7
marisoltorres99 <86800286+marisoltorres99@users.noreply.github.com>
2025-11-12 17:37:44 -0300

    creacion de clases

---
+++ GestionPedidos/Model/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionPedidos.Model
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string NombreCliente { get; set; }
+        public string Email { get; set; }
+        public List<Pedido> Pedidos { get; set; }
+    }
+}
---
+++ GestionPedidos/Model/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPedidos.Model
+{
+    public class Pedido
+    {
+        public int IdPedido { get; set; }
+        public int IdCliente { get; set; }
+        public decimal MontoBase { get; set; }
+        public TiposDeEnvio TipoEnvio { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+
+    public enum TiposDeEnvio
+    {
+        Estandar,
+        Express,
+        Internacional
+    }
+}
